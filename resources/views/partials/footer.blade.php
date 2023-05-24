98d1f32d5a00ef14440f65fef688889cf9b1e605
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-05-24 19:06:08 +0200

    ADD accents on header and footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="text-center bg-dark py-3 text-light">
-  Made with &hearts; by Mattia Volpe - Copyright &copy; {{date("Y")}}
+  <p class="mb-0 rounded-2 border-light mx-auto px-3 py-2">Made with <span class="text-danger">&hearts;</span> by Mattia Volpe - Copyright &copy; {{date("Y")}}</p>
 </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header class="text-center bg-dark py-3">
   <img height="150" src="{{ Vite::asset('resources/img/logo.png') }}" alt="The Movies Showcase logo">
   <nav class="nav justify-content-center">
-    <a class="nav-link text-light mx-2 {{ Route::currentRouteName() === 'home' ? 'active' : '' }}" href="{{ route('home') }}">Home</a>
-    <a class="nav-link text-light mx-2 {{ Route::currentRouteName() === 'contactUs' ? 'active' : '' }}" href="{{ route('contactUs') }}">Contact Us</a>
+    <a class="nav-link text-light mx-2 rounded-2 {{ Route::currentRouteName() === 'home' ? 'active' : '' }}" href="{{ route('home') }}">Home</a>
+    <a class="nav-link text-light mx-2 rounded-2 {{ Route::currentRouteName() === 'contactUs' ? 'active' : '' }}" href="{{ route('contactUs') }}">Contact Us</a>
   </nav>
 </header>
