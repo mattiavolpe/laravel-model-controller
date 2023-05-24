60d2dc088ba71ab2ffda2041a4d60e3dffa2de61
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-05-24 18:26:21 +0200

    ADD move movies and contact us page

--- resources/views/contactUs.blade.php
+++ resources/views/contactUs.blade.php
@@ -1,4 +1,37 @@
 @extends("layouts.app")
 
 @section("content")
+<div class="container">
+  <h1 class="text-center text-light">Contact Us</h1>
+  <form class="w-50 mx-auto">
+    <div class="pb-3">
+      <label for="name" class="form-label text-light">Name</label>
+      <input type="text" name="name" id="name" class="form-control" placeholder="Your name..." aria-describedby="nameHelper">
+      <small id="nameHelper" class="form-text text-muted">Type your name here</small>
+    </div>
+    <div class="pb-3">
+      <label for="email" class="form-label text-light">E-mail</label>
+      <input type="email"
+        class="form-control" name="email" id="email" aria-describedby="emailHelper" placeholder="Your e-mail...">
+      <small id="emailHelper" class="form-text text-muted">Type your e-mail here</small>
+    </div>
+    <div class="pb-3">
+      <label for="confirmEmail" class="form-label text-light">Confirm e-mail</label>
+      <input type="email"
+        class="form-control" name="confirmEmail" id="confirmEmail" aria-describedby="confirmEmailHelper" placeholder="Confirm your e-mail...">
+      <small id="emailHelper" class="form-text text-muted">Type again your e-mail here</small>
+    </div>
+    <div class="pb-3">
+      <label for="message" class="form-label text-light">Message</label>
+      <br>
+      <textarea class="w-100 rounded-2" name="message" id="message" rows="10" placeholder="Your message..."></textarea>
+      <br>
+      <small id="messageHelper" class="form-text text-muted">Type your message here</small>
+    </div>
+    <div class="text-center">
+      <button type="submit" class="btn btn-danger text-light mx-2">Send</button>
+      <button type="reset" class="btn btn-warning text-dark mx-2">Reset</button>
+    </div>
+  </form>
+</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,8 +7,10 @@
 
     <title>The Movies Showcase</title>
 
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <!-- GOOGLE FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@400;500;700;900&display=swap" rel="stylesheet">
 
     <!-- FONTAWESOME -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer">
